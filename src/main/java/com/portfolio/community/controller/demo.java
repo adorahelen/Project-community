a956b8d9d5ab7bd1de02e36da2398b5479c644de
adorahelen <adorahelenmin@gmail.com>
2024-08-17 15:40:38 +0900

    Front : layout templates

--- src/main/java/com/portfolio/community/controller/demo.java
+++ src/main/java/com/portfolio/community/controller/demo.java
@@ -12,5 +12,14 @@ public class demo {
                 // 앞에가 프론트에서 지정한 "이름" : 뒤에는 변수 값 "보통은 JSON 객체"
         return "greetings"; // 템플릿 디렉토리 안에 있는, 뷰 파일 반환
     }
+
+
+
+    @GetMapping("/start2") // 해당하는 유엘알 요청 링크 접속시 아래에 함수 실행
+    public String by(Model model) { // 모델 객체 가져오기
+        model.addAttribute("username", "administrator");
+        // 앞에가 프론트에서 지정한 "이름" : 뒤에는 변수 값 "보통은 JSON 객체"
+        return "sogang"; // 템플릿 디렉토리 안에 있는, 뷰 파일 반환
+    }
 }
 // Model View Controller : MVC 는 데이터를 담는 모델, 반환해서 보여주는 뷰, 행동하는 컨트롤러
