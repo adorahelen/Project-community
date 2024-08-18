9291469a72886a574da7315fea086ae41cf1f6ec
adorahelen <adorahelenmin@gmail.com>
2024-08-18 16:04:57 +0900

    단일 데이터 조회

--- src/main/java/com/portfolio/community/controller/ArticleController.java
+++ src/main/java/com/portfolio/community/controller/ArticleController.java
@@ -6,7 +6,9 @@ import com.portfolio.community.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -24,19 +26,28 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         log.info(form.toString());
-//        System.out.println(form.toString());
         // 앞으로 할 작업
         // 1. DTO를 entitiy로 변환
         // 2. Repository로 entitiy를 DB에 저장
             Article article = form.toEntity();
             log.info(article.toString());
-//        System.out.println(article.toString());
 
             Article saved = articleRepository.save(article);
             log.info(saved.toString());
-//        System.out.println(saved.toString());
-            // 아티클 엔티티를 저장해, saved 객체에 반환한다.
-
-        return " "; // 데이터는 콘솔에 찍히지만, 404만 찍힌다.(메소드 자체가 있든 없든)
+        return "";
     }
+
+    @GetMapping("articles/{id}") // 데이터 요청 접수
+    public String show(@PathVariable Long id, Model model) {
+        // @PathVariable : URL 요청으로 들어온 전달값을 컨트롤러의 매개변수로 가져온다. (Long id를 {id}로)
+        log.info("id: " + id);
+        // 0. 클라이언트가 접속하는 URL 요청을 컨트롤러에서 받기 ("article -> log print)
+        // 1. 해당하는 아이디를 조회해 데이터를 가져오기 : Repository
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        // 3. 뷰 페이지 제작하여 반환하기
+        return "articles/show";
+    }
+
 }
--- src/main/java/com/portfolio/community/entitiy/Article.java
+++ src/main/java/com/portfolio/community/entitiy/Article.java
@@ -5,12 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
-@Entity // JPA에서 제공하는 @로, 붙은 클래스를 기반으로 DB에 table을 생성한다. tabel name == class name
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
+// JPA에서 제공하는 @로, 붙은 클래스를 기반으로 DB에 table을 생성한다. tabel name == class name
 public class Article {
-    public Article() {
-
-    }
-
     @Id // primary key : Auto Increment
     @GeneratedValue
     private Long id;
@@ -20,20 +24,5 @@ public class Article {
 
     @Column
     private String content;
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
 
